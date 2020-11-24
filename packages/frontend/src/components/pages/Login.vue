d3518b0c93bba889018bb1c639646881073e1f16
SJ <stevenje@outlook.com>
2020-11-24 08:49:49 -0500

    add signup / login links

--- packages/frontend/src/components/pages/Login.vue
+++ packages/frontend/src/components/pages/Login.vue
@@ -10,7 +10,9 @@
         <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
         <p class="mt-2 text-center text-sm text-gray-600">
           Or
-          <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> start your 14-day free trial </a>
+          <router-link to="/signup">
+            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> start your 14-day free trial </a>
+          </router-link>
         </p>
       </div>
       <form class="mt-8 space-y-6" action="#" method="POST" @submit.prevent="login()">
--- packages/frontend/src/components/pages/Signup.vue
+++ packages/frontend/src/components/pages/Signup.vue
@@ -10,7 +10,9 @@
         <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Start your 14-day free trial</h2>
         <p class="mt-2 text-center text-sm text-gray-600">
           Or
-          <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> sign in to your account </a>
+          <router-link to="/login">
+            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> sign in to your account </a>
+          </router-link>
         </p>
       </div>
       <form class="mt-8 space-y-6" action="#" method="POST" @submit.prevent="signup()">
