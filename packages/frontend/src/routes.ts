840ab244f67b25272b56b851c62375d9c3a9458d
SJ <stevenje@outlook.com>
2020-11-24 06:16:26 -0500

    Login Page Route + HTML

--- packages/frontend/src/routes.ts
+++ packages/frontend/src/routes.ts
@@ -1,5 +1,6 @@
 import { RouteRecordRaw } from "vue-router";
 import Home from "/@/components/pages/Home.vue";
+import Login from "/@/components/pages/Login.vue";
 import NotFound from "/@/components/pages/NotFound.vue";
 
 export const routes: RouteRecordRaw[] = [
@@ -7,6 +8,11 @@ export const routes: RouteRecordRaw[] = [
     path: "/",
     component: Home,
     meta: { title: "Home" },
+  },
+  {
+    path: "/login",
+    component: Login,
+    meta: { title: "Login" },
   },
   {
     path: "/:path(.*)",
