09898937fbe7ee81ba10dab25afce5c05b4134cf
Pascal Martineau <pascal@lewebsimple.ca>
2021-02-05 11:28:15 -0500

    Remove vite-plugin-voie

--- client/web/src/router.ts
+++ client/web/src/router.ts
@@ -1,5 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
-import routes from "voie-pages";
+
+import Home from "./pages/index.vue";
+import Login from "./pages/login.vue";
+import NotFound from "./pages/[...all].vue";
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/login", component: Login },
+  { path: "/:pathMatch(.*)*", component: NotFound },
+];
 
 export const router = createRouter({
   history: createWebHistory(),
