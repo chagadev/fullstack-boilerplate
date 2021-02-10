22712f15378011fe4f10d32292f60944f7590a85
Pascal Martineau <pascal@lewebsimple.ca>
2021-02-10 15:02:57 -0500

    WIP nuxt

--- client/nuxt/nuxt.config.js
+++ client/nuxt/nuxt.config.js
@@ -2,5 +2,6 @@ export default {
   buildDir: "../../.nuxt",
   buildModules: ["@nuxtjs/composition-api", "@nuxt/typescript-build", "@nuxtjs/tailwindcss"],
   css: ["@braid/vue-formulate/dist/snow.css"],
+  modules: ["@nuxt/http"],
   plugins: ["~/plugins/vue-formulate.ts", "~/plugins/villus.ts"],
 };
--- server/backend/index.ts
+++ server/backend/index.ts
@@ -9,4 +9,4 @@ export const app = fastify({});
 app.register(authJwtPlugin);
 app.register(authLocalPlugin);
 app.register(mercuriusPlugin);
-app.register(nuxtPlugin);
+//app.register(nuxtPlugin);
